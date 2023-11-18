ed7e6dd2501eff68313830e612a7aa83ead6c087
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-11-18 19:47:38 +0530

    added changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,6 +40,9 @@
 			console.log(e);
 		}
 	};
+
+
+	let btntext="COPY URL"
 </script>
 
 <div id="app" class="h-screen flex text-center my-10">
@@ -80,9 +83,17 @@
 		{/if}
 		<button
 			use:copy={retriveimg}
+			on:click={() => {
+				btntext="COPIED"
+				setTimeout(() => {
+					btntext="COPY URL"
+				}, 1000);
+			}}
 			class="rounded-md bg-gray-900 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900 m-3 h-fit"
 		>
-			COPY URL
+			{btntext}
+			
+
 		</button>
 	</div>
 </div>
